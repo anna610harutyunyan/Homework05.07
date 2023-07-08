d27fde02ecd530b2cbb4e1826349cca279c855bb
anna610harutyunyan <135502002+anna610harutyunyan@users.noreply.github.com>
2023-07-09 01:47:18 +0400

    Update Car.cpp

--- Car.cpp
+++ Car.cpp
@@ -1,91 +1,102 @@
 #include <iostream>
 #include <string>
 
-enum Colour
-{
-	red,
-	black,
-	white,
-	yellow,
-	green,
-	gray,
-	blue,
-	purple,
-	pink,
-	brown
-
-};
-
-class Car 
-{
-private:
-Colour m_colour;
+class Car {
 public:
-	std::string enum_to_string( Colour m_colour)
-	{
-		switch ( m_colour)
-		{
-		case red:
-			return "Red";
-			break;
-		case black:			
-			return "Black";
-			break;
-		case white:
-			return "White";
-			break;
-		case yellow:		
-			return "Yellow";
-			break;
-		case green:
-			return "Green";
-			break;
-		case gray:
-			return "Gray";
-			break;
-		case blue:
-			return "Blue";
-			break;
-		case purple:
-			return "Purple";
-			break;
-		case pink:
-			return "Pink";
-			break;
-		case brown:
-			return "Brown";
-			break;
-		default:
-			return "Unknown colour";
-			break;
-		}
-	}
+    enum Colour {
+        red,
+        black,
+        white,
+        yellow,
+        green,
+        gray,
+        blue,
+        purple,
+        pink,
+        brown
+    };
 
+private:
+    Colour m_colour;
 
-	Car() 
-	{
-		std::cout << __func__ << std::endl;
-	}
-	Car(Colour m_colour):m_colour(m_colour)
-	{
-	    std::cout <<__func__<<std::endl;
-	}
-	Colour get_number()const
-	{
-	    return m_colour;
-	}
+public:
+    static std::string enum_to_string(Colour m_colour) {
+        switch (m_colour) {
+            case red:
+                return "Red";
+            case black:
+                return "Black";
+            case white:
+                return "White";
+            case yellow:
+                return "Yellow";
+            case green:
+                return "Green";
+            case gray:
+                return "Gray";
+            case blue:
+                return "Blue";
+            case purple:
+                return "Purple";
+            case pink:
+                return "Pink";
+            case brown:
+                return "Brown";
+            default:
+                return "Unknown colour";
+        }
+    }
 
-	~Car()
-	{
-		std::cout << __func__ << std::endl;
-	}
+    static Colour string_to_enum(const std::string& initColour) {
+        if (initColour == "Red")
+            return red;
+        else if (initColour == "Black")
+            return black;
+        else if (initColour == "White")
+            return white;
+        else if (initColour == "Yellow")
+            return yellow;
+        else if (initColour == "Green")
+            return green;
+        else if (initColour == "Gray")
+            return gray;
+        else if (initColour == "Blue")
+            return blue;
+        else if (initColour == "Purple")
+            return purple;
+        else if (initColour == "Pink")
+            return pink;
+        else if (initColour == "Brown")
+            return brown;
+        else
+            return brown ; 
+    }
+
+    Car() {
+        std::cout << __func__ << std::endl;
+    }
+
+    Car(Colour colour) : m_colour(colour) {
+        std::cout << __func__ << std::endl;
+    }
+
+    Colour get_colour() const {
+        return m_colour;
+    }
+
+    ~Car() {
+        std::cout << __func__ << std::endl;
+    }
 };
 
-int main(int argc, char** argv)
-{
-	Car car;
-	std::cout <<car.enum_to_string(blue) << std::endl;
-	Car car1(blue);
-	std::cout <<car1.get_number() <<std::endl;
-	return 0;
+int main() {
+    Car car;
+    std::cout << Car::enum_to_string(Car::blue) << std::endl;
+    Car car1(Car::blue);
+    std::cout << car1.get_colour() << std::endl;
+
+    Car::Colour enumValue = Car::string_to_enum("Blue");
+    std::cout << enumValue << std::endl;
+
+    return 0;
 }
